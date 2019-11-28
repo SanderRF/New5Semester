12f59074acfd03f041b97c0f61b0ae9e7664fcaf
Sander Rask Finnemann <sand4384@edu.easj.dk>
2019-11-28 10:29:38 +0000

    Test1

---
+++ 5Semester/5Semester/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _5Semester.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
